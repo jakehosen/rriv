edea007180e95b9927a8d6cd9eaad37e3030a7d5
Your Name <jakehosen@gmail.com>
2021-10-03 14:28:23 -0400

    adding drivers and initial code for reading DHT22 humidity sensor

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,25 @@
 #include <string.h>
 #include <Ezo_i2c.h>
 
+//DHT22 humidity sensor stuff
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN PC11     // Digital pin connected to the DHT sensor 
+#define DHTTYPE DHT22     // DHT 22 (AM2302)
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+//end DHT22 stuff
+
 #include "datalogger.h"
 #include "scratch/dbgmcu.h"
 #include "system/watchdog.h"
 #include "sensors/atlas_rgb.h"
 #include "utilities/qos.h"
 #include "system/hardware.h"
+
 
 // Setup and Loop
 
@@ -90,6 +103,35 @@ void setup(void)
   Serial2.flush();
   
   print_debug_status(); 
+
+  //DHT22 Stuff
+    // Initialize device.
+  dht.begin();
+  Serial.println(F("DHT22 Initializing"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  
 }
 
 /* main run loop order of operation: */
@@ -116,6 +158,33 @@ void loop(void)
   bool debugLoop = checkDebugLoop();
   bool awakeForUserInteraction = checkAwakeForUserInteraction(debugLoop);
   bool takeMeasurement = checkTakeMeasurement(bursting, awakeForUserInteraction);
+
+  //Adding DHT test here
+   // Delay between measurements.
+  //delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  //if (isnan(event.temperature)) {
+  //  Serial.println(F("Error reading temperature!"));
+  //}
+  //else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  //}
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+  //End of DHT test code
+
 
   // Should we sleep until a measurement is triggered?
   bool awaitMeasurementTrigger = false;
